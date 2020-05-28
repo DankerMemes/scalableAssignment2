019dfc6380b05db4f0ec32f672a5d8dc572139bd
DankerMemes <zaryafs@gmail.com>
2020-05-27 23:20:56 -0400

    update: sockets work with nginx now

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+
+        };
+        res.writeHead(200,headers);
+        res.end();
+    }
+});
 var path = require('path');
 var {PythonShell} = require('python-shell');
 PythonShell.defaultOptions = { scriptPath: './python'}
